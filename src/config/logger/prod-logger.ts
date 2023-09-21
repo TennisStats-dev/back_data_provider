e8c570efad8a1d3932375581f640ee24b22a0974
crobledor <crobledoramos@gmail.com>
2023-09-21 16:51:28 +0200

    Delete Winstons mongoDB func
    As it is causing a problem when deploying, the func has been commented
    for now.

--- src/config/logger/prod-logger.ts
+++ src/config/logger/prod-logger.ts
@@ -1,7 +1,6 @@
 import type winston from 'winston'
 import { format, createLogger, transports } from 'winston'
-import { MongoDB } from 'winston-mongoDB'
-// import config from '..'
+// import { MongoDB } from 'winston-mongoDB'
 const { timestamp, combine, errors, json } = format
 
 export function buildProdLogger(): winston.Logger {
@@ -10,12 +9,12 @@ export function buildProdLogger(): winston.Logger {
 		defaultMeta: { service: 'database-backend-service' },
 		transports: [
             new transports.Console({level: 'info'}),
-            new MongoDB({
-                level: 'warn',
-                db: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-                options: { useUnifiedTopology: true, },
-                collection: 'serverdata'
-            }),
+            // new MongoDB({
+            //     level: 'warn',
+            //     db: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+            //     options: { useUnifiedTopology: true, },
+            //     collection: 'serverdata'
+            // }),
         ],
 	})
 }
